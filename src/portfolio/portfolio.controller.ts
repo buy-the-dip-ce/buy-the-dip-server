dcc2691148c2d81a57a49dbf34cc3f3dd6c7a447
jerrynim <tjerry3@naver.com>
2022-03-23 00:11:24 +0900

    promise all 적용

--- src/portfolio/portfolio.controller.ts
+++ src/portfolio/portfolio.controller.ts
@@ -1,30 +1,27 @@
-import { Categories } from "./../categories/categories.entity";
-import { PortfolioService } from "./portfolio.service";
-import { Portfolio } from "./portfolio.entity";
-import { Param, Post, Body } from "@nestjs/common";
-import { Controller, Get } from "@nestjs/common";
+import { Categories } from "./../categories/categories.entity"
+import { PortfolioService } from "./portfolio.service"
+import { Portfolio } from "./portfolio.entity"
+import { Param, Post, Body } from "@nestjs/common"
+import { Controller, Get } from "@nestjs/common"
 
 export type CreatePortfolioBody = {
-  [key: string]: string[];
-};
+    [key: string]: string[]
+}
 
 @Controller("/portfolios")
 export class PortfolioController {
-  constructor(private portfolioService: PortfolioService) {}
+    constructor(private portfolioService: PortfolioService) {}
 
-  @Post()
-  async savePortfolio(@Body() body: CreatePortfolioBody) {
-    const newPortfolio = await this.portfolioService.createPortfolio(body);
-    console.log("바디", body);
-    console.log("콘트롤러", newPortfolio);
-    return newPortfolio;
-  }
+    @Post()
+    async savePortfolio(@Body() body: CreatePortfolioBody) {
+        const newPortfolio = await this.portfolioService.createPortfolio(body)
 
-  @Get("/:id")
-  async getOne(@Param("id") id: string): Promise<Portfolio> {
-    console.log("get start");
-    const data = await this.portfolioService.findOne(id);
-    console.log(data);
-    return data;
-  }
+        return newPortfolio
+    }
+
+    @Get("/:id")
+    async getOne(@Param("id") id: string) {
+        const data = await this.portfolioService.findOne(id)
+        return data
+    }
 }
