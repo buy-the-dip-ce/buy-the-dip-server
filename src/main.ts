3c8e0a087fb647d9e7ce278b7edecc09c42dd2f1
jerrynim <tjerry3@naver.com>
2022-03-28 20:25:43 +0900

    add enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,48 @@
-import { Client } from "@elastic/elasticsearch";
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import path from "path";
-import fs from "fs";
-import dotenv from "dotenv";
-dotenv.config();
+import { NestFactory } from "@nestjs/core"
+import { AppModule } from "./app.module"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
+import path from "path"
+import fs from "fs"
+import dotenv from "dotenv"
+dotenv.config()
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        logger: ["log", "error", "debug", "verbose", "warn"],
+    })
 
-  if (process.env.STAGE === "dev") {
-    const options = new DocumentBuilder()
-      .setTitle("Buy The Dip API Docs")
-      .setDescription(
-        "Buy The Dip API Swagger 입니다.\nAuthorization이 필요없습니다."
-      )
-      .setVersion("1.0.0")
-      .addBearerAuth({
-        type: "http",
-        scheme: "bearer",
-        in: "header",
-        name: "Authorization",
-      })
-      .addServer(String("http://localhost:4000"), String("local"))
-      .build();
+    app.enableCors()
 
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup("docs", app, document);
+    if (process.env.STAGE === "dev") {
+        const options = new DocumentBuilder()
+            .setTitle("Buy The Dip API Docs")
+            .setDescription(
+                "Buy The Dip API Swagger 입니다.\nAuthorization이 필요없습니다."
+            )
+            .setVersion("1.0.0")
+            .addBearerAuth({
+                type: "http",
+                scheme: "bearer",
+                in: "header",
+                name: "Authorization",
+            })
+            .addServer(String("http://localhost:4000"), String("local"))
+            .build()
 
-    const basePath = "./swagger";
-    const swaggerSpecPath = path.join(basePath, "swagger-spec.json");
-    try {
-      fs.mkdirSync(basePath);
-    } catch (e) {}
-    fs.writeFileSync(
-      swaggerSpecPath,
-      JSON.stringify(document, undefined, "\t") + "\n"
-    );
-  }
+        const document = SwaggerModule.createDocument(app, options)
+        SwaggerModule.setup("docs", app, document)
 
-  await app.listen(4000);
+        const basePath = "./swagger"
+        const swaggerSpecPath = path.join(basePath, "swagger-spec.json")
+        try {
+            fs.mkdirSync(basePath)
+        } catch (e) {}
+        fs.writeFileSync(
+            swaggerSpecPath,
+            JSON.stringify(document, undefined, "\t") + "\n"
+        )
+    }
+
+    await app.listen(4000)
 }
-bootstrap();
+bootstrap()
