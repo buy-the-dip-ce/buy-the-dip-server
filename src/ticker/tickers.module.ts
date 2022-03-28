ceee9e47e7573352a4f07ac4fca2ff9a13dbd904
jerrynim <tjerry3@naver.com>
2022-03-28 21:18:36 +0900

    절대경로 제거

--- src/ticker/tickers.module.ts
+++ src/ticker/tickers.module.ts
@@ -1,12 +1,12 @@
-import { TickerService } from "src/ticker/tickers.service";
-import { TickerController } from "./ticker.controller";
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Ticker } from "./ticker.entity";
+import { TickerController } from "./ticker.controller"
+import { Module } from "@nestjs/common"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { Ticker } from "./ticker.entity"
+import { TickerService } from "./tickers.service"
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ticker])],
-  controllers: [TickerController],
-  providers: [TickerService],
+    imports: [TypeOrmModule.forFeature([Ticker])],
+    controllers: [TickerController],
+    providers: [TickerService],
 })
 export class TickerModule {}
--- src/ticker/tickers.service.ts
+++ src/ticker/tickers.service.ts
@@ -1,52 +1,52 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { Ticker } from "./ticker.entity";
-import client from "src/connection";
+import { Injectable } from "@nestjs/common"
+import { InjectRepository } from "@nestjs/typeorm"
+import { Repository } from "typeorm"
+import { Ticker } from "./ticker.entity"
+import client from "../connection"
 
 type tickerDataType = {
-  [key: string]: string[];
-};
+    [key: string]: string[]
+}
 @Injectable()
 export class TickerService {
-  constructor(
-    @InjectRepository(Ticker)
-    private tickersRepository: Repository<Ticker>
-  ) {}
+    constructor(
+        @InjectRepository(Ticker)
+        private tickersRepository: Repository<Ticker>
+    ) {}
 
-  findAll(): Promise<Ticker[]> {
-    return this.tickersRepository.find();
-  }
-
-  async find(keyword): Promise<tickerDataType[]> {
-    const query = keyword;
-    const elastic = client;
-    try {
-      const response = await elastic.search({
-        index: "tickers",
-        query: {
-          match: {
-            symbol: keyword,
-          },
-        },
-      });
-      const data = response.hits.hits.map((row) => {
-        return {
-          symbol: row._source["symbol"],
-          name: row._source["name"],
-        };
-      });
-      return data;
-    } catch (error) {
-      console.log(error);
+    findAll(): Promise<Ticker[]> {
+        return this.tickersRepository.find()
     }
-  }
 
-  findOne(id: string): Promise<Ticker> {
-    return this.tickersRepository.findOne(id);
-  }
+    async find(keyword): Promise<tickerDataType[]> {
+        const query = keyword
+        const elastic = client
+        try {
+            const response = await elastic.search({
+                index: "tickers",
+                query: {
+                    match: {
+                        symbol: keyword,
+                    },
+                },
+            })
+            const data = response.hits.hits.map((row) => {
+                return {
+                    symbol: row._source["symbol"],
+                    name: row._source["name"],
+                }
+            })
+            return data
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
-  async remove(id: string): Promise<void> {
-    await this.tickersRepository.delete(id);
-  }
+    findOne(id: string): Promise<Ticker> {
+        return this.tickersRepository.findOne(id)
+    }
+
+    async remove(id: string): Promise<void> {
+        await this.tickersRepository.delete(id)
+    }
 }
