e138bb8188da6db5d951cef54e33b895032ccbb1
jerrynim <tjerry3@naver.com>
2022-03-30 21:43:48 +0900

    configuration 수정

--- script/getHistoricalData.ts
+++ script/getHistoricalData.ts
@@ -27,55 +27,58 @@ const run = async () => {
     const errorList = [];
     const res = [];
     let i = 0;
-    tickers.forEach(async (ticker) => {
-      try {
-        const {
-          data: { data: company },
-        } = await axios.get<any>(
-          `https://api.nasdaq.com/api/quote/${ticker.symbol.replace("/", ".")}/summary?assetclass=stocks`
-        );
-        i = i + 1;
-        if (company === null) {
-          return;
-        }
-        const market_cap = Number((company.summaryData.MarketCap.value.replace(/,/g, "") / 100000000000).toFixed(2));
-        const sector = company.summaryData.Sector.value;
-        const industry = company.summaryData.Industry.value;
-        const per = company.summaryData.PERatio.value;
-        const high_52 = company.summaryData.FiftTwoWeekHighLow.value.replace(/$,/g, "").split("/")[0].replace("$", "");
-        let _ticker = new Ticker();
-        _ticker.symbol = ticker.symbol;
-        _ticker.name = ticker.name;
-        _ticker.country = !!ticker.country ? ticker.country : null;
-        _ticker.market_cap = !!market_cap ? market_cap : 0;
-        _ticker.per = !!per && per !== "N/A" ? per : 0;
-        _ticker.high_52 = Number(high_52) || 0;
-        _ticker.sector = !!sector && sector !== "N/A" ? sector : null;
-        _ticker.industry = !!industry && industry !== "N/A" ? industry : null;
-        const finds = await connection.manager.find(Ticker, { where: { symbol: "Asds" } });
-        if (finds.length === 0) {
-          await connection.manager.save(Ticker, _ticker);
-        }
-        await connection.manager.update(Ticker, ticker.symbol, _ticker);
+    const filters = ["CMG", "BRK/A", "GOOGL"];
+    tickers
+      .filter((_t) => filters.includes(_t.symbol))
+      .forEach(async (ticker) => {
+        try {
+          const {
+            data: { data: company },
+          } = await axios.get<any>(`https://api.nasdaq.com/api/quote/${ticker.symbol}/summary?assetclass=stocks`);
+          i = i + 1;
+          if (company === null) {
+            return;
+          }
+          const market_cap = Number((company.summaryData.MarketCap.value.replace(/,/g, "") / 100000000000).toFixed(2));
+          const sector = company.summaryData.Sector.value;
+          const industry = company.summaryData.Industry.value;
+          const per = company.summaryData.PERatio.value;
+          const high_52 = company.summaryData.FiftTwoWeekHighLow.value.replace(/,/g, "").split("/")[0].replace("$", "");
+          console.log(high_52, company.summaryData.FiftTwoWeekHighLow.value);
+          let _ticker = new Ticker();
+          _ticker.symbol = ticker.symbol;
+          _ticker.name = ticker.name;
+          _ticker.country = !!ticker.country ? ticker.country : null;
+          _ticker.market_cap = !!market_cap ? market_cap : 0;
+          _ticker.per = !!per && per !== "N/A" ? per : 0;
+          _ticker.high_52 = Number(high_52) || 0;
+          _ticker.sector = !!sector && sector !== "N/A" ? sector : null;
+          _ticker.industry = !!industry && industry !== "N/A" ? industry : null;
+          console.log(_ticker);
+          const finds = await connection.manager.find(Ticker, { where: { symbol: "Asds" } });
+          if (finds.length === 0) {
+            await connection.manager.save(Ticker, _ticker);
+          }
+          await connection.manager.update(Ticker, ticker.symbol, _ticker);
 
-        console.log(ticker.symbol, " Added");
-      } catch (e) {
-        if (e.code !== "ETIMEDOUT") {
-          console.log(ticker.symbol, " ", e.message, e.code);
+          console.log(ticker.symbol, " Added");
+        } catch (e) {
+          if (e.code !== "ETIMEDOUT") {
+            console.log(ticker.symbol, " ", e.message, e.code);
+          }
+          if (e?.code !== "ER_DUP_ENTRY") {
+            errorList.push(JSON.stringify(ticker));
+          }
+        } finally {
+          console.log(i);
+          if (ticker.symbol === "ZYXI") {
+            setTimeout(() => {
+              fs.writeFileSync("static/tickers.ts", `export const tickers =[${errorList}];`);
+              process.exit(1);
+            }, 0);
+          }
         }
-        if (e?.code !== "ER_DUP_ENTRY") {
-          errorList.push(JSON.stringify(ticker));
-        }
-      } finally {
-        console.log(i);
-        if (ticker.symbol === "ZYXI") {
-          setTimeout(() => {
-            fs.writeFileSync("static/tickers.ts", `export const tickers =[${errorList}];`);
-            process.exit(1);
-          }, 0);
-        }
-      }
-    });
+      });
   } catch (e) {
     console.debug(e);
   }
