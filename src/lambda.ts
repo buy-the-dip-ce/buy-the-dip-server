5778a14657393d6dd296bdb35f3388e6f74731eb
jerrynim <tjerry3@naver.com>
2022-03-30 23:44:16 +0900

    lambda cors 수정

--- src/lambda.ts
+++ src/lambda.ts
@@ -25,6 +25,7 @@ async function bootstrapServer(): Promise<Server> {
             AppModule,
             new ExpressAdapter(expressApp)
         )
+        nestApp.enableCors()
         nestApp.use(eventContext())
         await nestApp.init()
         cachedServer = createServer(expressApp, undefined, binaryMimeTypes)
